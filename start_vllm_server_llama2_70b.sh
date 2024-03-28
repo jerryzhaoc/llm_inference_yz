eb88b8131e55d08a07d32c03eb81aa5fa8d45f04
jerryzhaoc <132997627+jerryzhaoc@users.noreply.github.com>
2024-03-28 13:42:21 +0800

    Update start_vllm_server_llama2_70b.sh

--- start_vllm_server_llama2_70b.sh
+++ start_vllm_server_llama2_70b.sh
@@ -1,8 +1,8 @@
 python -m vllm.entrypoints.openai.api_server \
---model /disk1/jerry.zhao/models/Llama2-70B-chat-AWQ \
---served-model-name Llama2-70B-chat-AWQ \
+--model /root/autodl-tmp/TheBloke_Llama-2-70B-Chat-AWQ \
+--served-model-name Llama2-70B-Chat-AWQ \
 --trust-remote-code \
---max-model-len 2048 -q awq \
+--max-model-len 4096 -q awq \
 -tp 2 \
 --gpu-memory-utilization 1 \
 --enforce-eager
